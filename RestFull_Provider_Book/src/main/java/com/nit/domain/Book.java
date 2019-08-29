40978473657e23ee00a08478f8b6d78adb6b5748
anni <anni@gmail.com>
2019-08-29 17:40:46 +0530

    add getter and setter in domain classes
    by Anirudha

--- RestFull_Provider_Book/src/main/java/com/nit/domain/Book.java
+++ RestFull_Provider_Book/src/main/java/com/nit/domain/Book.java
@@ -6,4 +6,23 @@ public class Book {
 	private String name;
 	private Author author;
 	
+	public Integer getIsbn() {
+		return isbn;
+	}
+	public void setIsbn(Integer isbn) {
+		this.isbn = isbn;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Author getAuthor() {
+		return author;
+	}
+	public void setAuthor(Author author) {
+		this.author = author;
+	}
+	
 }
